4b18d23043c1867b706865290a8a3f1228ba1e02
Anurag kumar <Anuragkr8651@gmail.com>
2025-09-29 00:01:05 +0530

    added base url

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXTAUTH_URL || "http://localhost:3000",
-
+    baseURL: process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3000",
     headers: {
         "Content-Type": "application/json"
     }
-})
+});
 
-export default axiosInstance
+export default axiosInstance;
